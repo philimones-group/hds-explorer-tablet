84bc37fb84323895ebe95b29b3d47655f364bcde
Paulo Filimone <paulphilimone@gmail.com>
2016-08-10 20:43:05 +0200

    Add POMActivity and layout

---
+++ app/src/main/java/net/manhica/clip/explorer/main/POMActivity.java
@@ -0,0 +1,14 @@
+package net.manhica.clip.explorer.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+import net.manhica.clip.explorer.R;
+
+public class POMActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pom);
+    }
+}

c4fc22f880ca07c961b3b9b96f203428adca1e94
Paulo Filimone <paulphilimone@gmail.com>
2020-09-04 10:42:51 +0200

    Change TrackingList Main UI to NUI theme

--- app/src/main/java/org/philimone/hds/explorer/widget/CirclePercentageBar.java
+++ app/src/main/java/org/philimone/hds/explorer/widget/CirclePercentageBar.java
@@ -2,7 +2,10 @@ package org.philimone.hds.explorer.widget;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.PorterDuff;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
@@ -21,6 +24,10 @@ public class CirclePercentageBar extends LinearLayout {
     private TextView txtPercentageValue;
     private ProgressBar progressBar;
     private DisplayType displayType = DisplayType.PERCENTAGE;
+    private int displayTextColor = 0;
+    private int displayTextSize = 0;
+    private int displayCircleColor = 0;
+    private int displayPercentageValue = 0;
 
     public enum DisplayType {
         FRACTION, PERCENTAGE
@@ -43,15 +50,22 @@ public class CirclePercentageBar extends LinearLayout {
 
     private void loadAttributes(AttributeSet attrs){
         String displayTextType = "";
+
         TypedArray ta = this.getContext().obtainStyledAttributes(attrs, R.styleable.CirclePercentageBar, 0, 0);
 
         try {
             displayTextType = ta.getString(R.styleable.CirclePercentageBar_displayTextType);
-            if (displayTextType.equals("fraction")){
+            displayTextSize = ta.getInt(R.styleable.CirclePercentageBar_displayTextSize, 10);
+            displayTextColor = ta.getColor(R.styleable.CirclePercentageBar_displayTextColor, getResources().getColor(R.color.nui_color_circle_color_one, null));
+            displayCircleColor = ta.getColor(R.styleable.CirclePercentageBar_displayCircleColor, getResources().getColor(R.color.nui_color_circle_color_two, null));
+            displayPercentageValue = ta.getInt(R.styleable.CirclePercentageBar_displayPercentageValue, 0);
+
+            if (displayTextType!=null && displayTextType.equals("fraction")){
                 displayType = DisplayType.FRACTION;
             }else{
                 displayType = DisplayType.PERCENTAGE;
             }
+
         } finally {
             ta.recycle();
         }
@@ -67,7 +81,35 @@ public class CirclePercentageBar extends LinearLayout {
         this.txtPercentageValue = (TextView) view.findViewById(R.id.txtCircleBarValue);
         this.progressBar = (ProgressBar) view.findViewById(R.id.pBarCircleProgressBar);
 
-        updateTextView();
+        setAttributes();
+        updateViews();
+
+    }
+
+    private void setAttributes() {
+        if (displayTextSize != 0){
+            this.txtPercentageValue.setTextSize(TypedValue.COMPLEX_UNIT_SP,  displayTextSize*1.0F);
+        }
+
+        if (displayTextColor != 0){
+            this.txtPercentageValue.setTextColor(displayTextColor);
+        }
+
+        if (displayCircleColor != 0){
+            this.progressBar.getProgressDrawable().setColorFilter(displayCircleColor, PorterDuff.Mode.SRC_IN);
+        }
+
+        this.progressBar.setProgress(displayPercentageValue);
+
+        if (displayType==DisplayType.PERCENTAGE){
+            this.txtPercentageValue.setText(displayPercentageValue+"%");
+        }else{
+            this.txtPercentageValue.setText(displayPercentageValue+"/"+maxValue);
+        }
+
+        Log.d("text-size", ""+displayTextSize);
+        Log.d("text-color", ""+displayTextColor);
+        Log.d("circ-color", ""+displayCircleColor);
     }
 
     public int getValue() {
@@ -91,23 +133,26 @@ public class CirclePercentageBar extends LinearLayout {
     public void setPercentageValue(int value){
         this.percentageValue = value;
         this.progressBar.setProgress(value);
-        updateTextView();
+        updateViews();
     }
 
     private void calcPercentageByValues() {
         this.percentageValue = value/maxValue;
         this.progressBar.setProgress(this.percentageValue);
-        updateTextView();
+        updateViews();
     }
 
-    private void updateTextView(){
+    private void updateViews(){
+        updatePercentageValue();
+    }
+
+    private void updatePercentageValue(){
         if (displayType==DisplayType.PERCENTAGE){
             this.txtPercentageValue.setText(percentageValue+"%");
         }else{
             this.txtPercentageValue.setText(value+"/"+maxValue);
         }
     }
-
 
 
 }

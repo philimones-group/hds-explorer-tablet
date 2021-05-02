4ca6a021072347296ff15d692b6dc90ca4b69b6b
Paulo Filimone <paulphilimone@gmail.com>
2021-05-03 00:37:09 +0200

    Add ObjectBrowser for debugging (remove in the future)

--- app/src/main/java/org/philimone/hds/explorer/database/ObjectBoxDatabase.java
+++ app/src/main/java/org/philimone/hds/explorer/database/ObjectBoxDatabase.java
@@ -1,9 +1,12 @@
 package org.philimone.hds.explorer.database;
 
 import android.content.Context;
+import android.util.Log;
 
+import org.philimone.hds.explorer.BuildConfig;
 import org.philimone.hds.explorer.model.MyObjectBox;
 import io.objectbox.BoxStore;
+import io.objectbox.android.AndroidObjectBrowser;
 
 /*
  * Initialize ObjectBox Database
@@ -14,6 +17,11 @@ public class ObjectBoxDatabase {
 
     public static void init(Context context) {
         boxStore = MyObjectBox.builder().androidContext(context.getApplicationContext()).build();
+
+        if (BuildConfig.DEBUG) {
+            boolean started = new AndroidObjectBrowser(boxStore).start(context);
+            Log.i("ObjectBrowser", "Started: " + started);
+        }
     }
 
     public static BoxStore get() {
